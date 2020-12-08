e0130d1f6ebde0a62c8616e42084eae0d84a6baf
Aru Ushida <rx-78.aru0719@outlook.jp>
2020-12-08 12:38:44 +0900

    SDKキー変更

--- app2.js
+++ app2.js
@@ -15,31 +15,35 @@ var db = firebase.firestore();
     //オブジェクトの取得
 const txtEmail = document.getElementById('txtEmail');       //メール
 const txtPassword = document.getElementById('txtPassword'); //パスワード
-const btnLogin = document.getElementById('btnLogin');       //log inボタン
-const btnLogout = document.getElementById('btnLogout');     //Log outボタン 
+const btnSignUp = document.getElementById('btnSignUp');     //Sign Upボタン
     //log inボタンのclickイベント
     //クリックされた時に実行する処理
-btnLogin.addEventListener('click', e => {
+    
+    //Sign Upボタンのclickイベント
+    //クリックされた時に実行する処理
+btnSignUp.addEventListener('click', e => {
     var email = txtEmail.value;   //メールを取得
     var shaObj = new jsSHA("SHA-256","TEXT");
     shaObj.update(email);
     var hash = shaObj.getHash("HEX");
     localStorage.setItem('shamail',hash);
+    console.log(hash);
     const pass = txtPassword.value; //パスワードを取得
     const auth = firebase.auth();   //FirebaseAuthenticationを取得
-    const promise = auth.signInWithEmailAndPassword(email, pass);   //メールとパスワードでサインイン可
-    promise.catch(e => console.log(e.message)); //結果をconsoleに表示
-    console.log(hash);
-    var _mail = localStorage.getItem("shamail");
-    console.log(_mail);
-    //document.location.assign('singup.html');
+    var mail_2 = localStorage.getItem("shamail");
+    console.log(mail_2);
+    const promise = auth.createUserWithEmailAndPassword(email, pass);   //メールとパスワードでサインイン
+    db.collection(hash).doc("like").set({
+    })
+    db.collection(hash).doc("organizer").set({
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    });
+    
+    promise.catch(function(e){console.log(e.message)}); //結果をconsoleに表示
 });
    
-    //Log outボタンのclickイベント
-    //クリックされた時に実行する処理
-btnLogout.addEventListener('click', e =>   {
-    firebase.auth().signOut();  //サインアウト
-});
     
     //ログインしているユーザー情報を返す非同期（コールバック）処理
     //認証状態を取得
